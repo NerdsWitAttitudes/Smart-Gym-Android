d21af6d60eace812de72b37332c26f215cedb164
Robin Siep <RobinSiep1@gmail.com>
2016-05-25 20:15:17 +0200

    refactoring

--- app/src/main/java/com/nwa/smartgym/activities/Devices.java
+++ app/src/main/java/com/nwa/smartgym/activities/Devices.java
@@ -3,6 +3,7 @@ package com.nwa.smartgym.activities;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,19 +28,19 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class Devices extends ListActivity {
+public class Devices extends ListActivity{
 
     private ArrayAdapter<Device> viewAdapter;
+    private List<Device> devices;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("woohoo");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devices);
-        System.out.println("CALLED");
-        requestSavedDevices();
+        DeviceListTask deviceListTask = new DeviceListTask();
+        deviceListTask.execute((Void) null);
 
-        /**iewAdapter = new ArrayAdapter<Device>(this, R.layout.device_list_item, requestSavedDevices());
+        viewAdapter = new ArrayAdapter<>(this, R.layout.device_list_item, devices);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add_device);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -47,50 +48,52 @@ public class Devices extends ListActivity {
             public void onClick(View view) {
                 return;
             }
-        });**/
+        });
     }
 
-    private List<Device> requestSavedDevices() {
-        System.out.println("request called");
-        SecretsHelper secretsHelper = new SecretsHelper(this);
-        DeviceAPI deviceService = ServiceGenerator.createSmartGymService(DeviceAPI.class,
-                                                                         secretsHelper.getAuthToken());
-        System.out.println("made it");
-        Call<List<Device>> call = deviceService.listDevices();
-        call.enqueue(new Callback<List<Device>>() {
+    public class DeviceListTask extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        public Boolean doInBackground(Void... Params) {
+            SecretsHelper secretsHelper = new SecretsHelper(getApplicationContext());
+            DeviceAPI deviceService = ServiceGenerator.createSmartGymService(DeviceAPI.class,
+                    secretsHelper.getAuthToken());
+            Call<List<Device>> call = deviceService.listDevices();
+            call.enqueue(new Callback<List<Device>>() {
 
-            @Override
-            public void onResponse(Call<List<Device>> call, Response<List<Device>> response){
-                System.out.println("response code");
-                System.out.println(response.code());
-                if (response.code() == 200){
-                    System.out.println("fuck yea");
-                   return;
-                } else if (response.code() == 400){
-                    System.out.println("fuck no");
-                    return;
-                } else {
+                @Override
+                public void onResponse(Call<List<Device>> call, Response<List<Device>> response) {
                     System.out.println(response.code());
+                    if (response.code() == 200) {
+                        System.out.println("stupidshit");
+                        devices.addAll(response.body());
+                        System.out.println(devices.size());
+                    } else if (response.code() == 400) {
+                        System.out.println("this happened");
+                        return;
+                    } else {
+                        System.out.println("generic");
+                        System.out.println(response.code());
+                        raiseGenericError();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<Device>> call, Throwable t) {
+                    System.out.println("sowrong");
+                    System.out.println(t);
                     raiseGenericError();
                 }
-            }
 
-            @Override
-            public void onFailure(Call<List<Device>> call, Throwable t){
-                System.out.println(t);
-                raiseGenericError();
-            }
+                private void raiseGenericError() {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            getString(R.string.server_500_message),
+                            Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
 
-            private void raiseGenericError(){
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        getString(R.string.server_500_message),
-                        Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
+            return true;
+        }
 
-        return new ArrayList<Device>();
     }
-
-
 }
--- app/src/main/java/com/nwa/smartgym/models/Device.java
+++ app/src/main/java/com/nwa/smartgym/models/Device.java
@@ -4,11 +4,27 @@ package com.nwa.smartgym.models;
  * Created by robin on 24-5-16.
  */
 public class Device {
-    private String MACAddress;
+    private String deviceAddress;
     private String name;
 
-    public Device(String MACAddress, String name) {
-        this.MACAddress = MACAddress;
+    public Device(String device_address, String name) {
+        this.deviceAddress = device_address;
+        this.name = name;
+    }
+
+    public String getDeviceAddress() {
+        return deviceAddress;
+    }
+
+    public void setDeviceAddress(String deviceAddress) {
+        this.deviceAddress = deviceAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 }
