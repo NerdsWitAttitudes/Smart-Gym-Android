0145943200f19f0ad280e24cdb858767f6bd9409
Sander de Winter <sanderdewinter@gamil.com>
2016-05-26 08:52:06 +0200

    Serialization done

--- app/src/main/java/com/nwa/smartgym/models/SportSchedule.java
+++ app/src/main/java/com/nwa/smartgym/models/SportSchedule.java
@@ -1,19 +1,19 @@
 package com.nwa.smartgym.models;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
+import com.google.gson.annotations.SerializedName;
 
-import java.lang.reflect.Type;
 import java.util.UUID;
 
-public class SportSchedule implements JsonDeserializer<SportSchedule> {
+public class SportSchedule {
 
     private UUID id;
+
+    @SerializedName("user_id")
     private UUID userId;
+
     private String name;
+
+    @SerializedName("reminder_minutes")
     private int reminderMinutes;
 
     public SportSchedule(UUID id, UUID userId, String name, int reminderMinutes) {
@@ -25,17 +25,5 @@ public class SportSchedule implements JsonDeserializer<SportSchedule> {
 
     public SportSchedule() {
 
-    }
-
-    @Override
-    public String toString() {
-        return "Sportschema: " + this.name + " reminder: " + this.reminderMinutes + " door: " + this.userId;
-    }
-
-    @Override
-    public SportSchedule deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        JsonElement jsonElement = json.getAsJsonArray().get(0);
-
-        return new Gson().fromJson(jsonElement, SportSchedule.class);
     }
 }
