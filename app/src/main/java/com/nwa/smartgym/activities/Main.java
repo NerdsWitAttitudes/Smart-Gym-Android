72c2942caeca08260c8cc7a2399ac33794dc0d77
Robin Siep <RobinSiep1@gmail.com>
2016-05-30 20:34:36 +0200

    menu drawer

--- app/src/main/java/com/nwa/smartgym/activities/Main.java
+++ app/src/main/java/com/nwa/smartgym/activities/Main.java
@@ -1,10 +1,16 @@
 package com.nwa.smartgym.activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.nwa.smartgym.R;
 
@@ -12,11 +18,14 @@ import java.util.ArrayList;
 import java.util.List;
 import  com.nwa.smartgym.lib.DefaultPageAdapter;
 import  com.nwa.smartgym.lib.NonSwipeableViewPager;
+import com.nwa.smartgym.lib.adapters.DrawerAdapter;
+import com.nwa.smartgym.models.DrawerItem;
 
-public class Main extends FragmentActivity {
+public class Main extends AppCompatActivity {
 
     private static NonSwipeableViewPager mViewPager;
     private DefaultPageAdapter mPageAdapter;
+    private ListView menuDrawerList;
 
     private static Context context;
 
@@ -32,10 +41,27 @@ public class Main extends FragmentActivity {
 
         mViewPager = (NonSwipeableViewPager) findViewById(R.id.main_container);
         mViewPager.setAdapter(mPageAdapter);
+
+        setupDrawer();
     }
 
     private List<Fragment> listFragments() {
         List<Fragment> fragmentList = new ArrayList<Fragment>();
         return fragmentList;
+    }
+
+    private void setupDrawer() {
+        menuDrawerList = (ListView) findViewById(R.id.menu_drawer_list);
+        DrawerAdapter drawerAdapter = new DrawerAdapter(this, getDrawerItems());
+        menuDrawerList.setAdapter(drawerAdapter);
+    }
+
+    private List<DrawerItem> getDrawerItems() {
+        List<DrawerItem> drawerItems = new ArrayList<>();
+        drawerItems.add(new DrawerItem(
+                getResources().getDrawable(R.drawable.bluetooth),
+                getString(R.string.action_persist_device),
+                new Intent(this, Devices.class)));
+        return drawerItems;
     }
 }
---
+++ app/src/main/java/com/nwa/smartgym/lib/adapters/DrawerAdapter.java
@@ -0,0 +1,48 @@
+package com.nwa.smartgym.lib.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nwa.smartgym.R;
+import com.nwa.smartgym.models.DrawerItem;
+
+import java.util.List;
+
+/**
+ * Created by robin on 30-5-16.
+ */
+public class DrawerAdapter extends ArrayAdapter<DrawerItem> {
+
+    public DrawerAdapter(Context context, List<DrawerItem> objects) {
+        super(context, R.layout.add_device_list_item, objects);
+    }
+
+    public View getView(final int position, View view, ViewGroup parent) {
+        final DrawerItem drawerItem = (DrawerItem) getItem(position);
+
+        // define views
+        LayoutInflater layoutInflater = (LayoutInflater) this.getContext().getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        View listItem = layoutInflater.inflate(R.layout.drawer_list_item, null);
+
+        TextView name = (TextView) listItem.findViewById(R.id.menu_drawer_name);
+        ImageView image = (ImageView) listItem.findViewById(R.id.menu_drawer_icon);
+
+        name.setText(drawerItem.getName());
+        image.setImageDrawable(drawerItem.getIcon());
+
+        listItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getContext().startActivity(getItem(position).getIntent());
+            }
+        });
+
+        return listItem;
+    }
+}
