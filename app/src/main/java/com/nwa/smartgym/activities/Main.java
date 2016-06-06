35f1e68bc4839a01a79815be6478703a76438740
Sander de Winter <sanderdewinter@gamil.com>
2016-06-06 10:29:47 +0200

    Not needed

--- app/src/main/java/com/nwa/smartgym/activities/Main.java
+++ app/src/main/java/com/nwa/smartgym/activities/Main.java
@@ -2,30 +2,19 @@ package com.nwa.smartgym.activities;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
-
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.nwa.smartgym.R;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import  com.nwa.smartgym.lib.DefaultPageAdapter;
-import  com.nwa.smartgym.lib.NonSwipeableViewPager;
-import com.nwa.smartgym.lib.NotificationService;
+import com.nwa.smartgym.lib.DefaultPageAdapter;
+import com.nwa.smartgym.lib.NonSwipeableViewPager;
 import com.nwa.smartgym.lib.adapters.DrawerAdapter;
 import com.nwa.smartgym.models.DrawerItem;
 
-import org.joda.time.LocalDate;
-import org.joda.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends AppCompatActivity {
 
@@ -49,16 +38,6 @@ public class Main extends AppCompatActivity {
         mViewPager.setAdapter(mPageAdapter);
 
         setupDrawer();
-
-        List<LocalDate.Property> properties = new ArrayList<>();
-        properties.add(new LocalDate().withDayOfWeek(1).dayOfWeek());
-        properties.add(new LocalDate().withDayOfWeek(2).dayOfWeek());
-        com.nwa.smartgym.models.SportSchedule hardgaan = new com.nwa.smartgym.models.SportSchedule(UUID.randomUUID(), UUID.randomUUID(), "Hardgaan", 0, LocalTime.now().plusSeconds(5), properties, true);
-        NotificationService notificationService = new NotificationService(this);
-        notificationService.scheduleNotifications(hardgaan);
-        notificationService.cancelScheduledNotifications();
-        hardgaan.setTime(LocalTime.now().plusSeconds(120));
-        notificationService.scheduleNotifications(hardgaan);
     }
 
     private List<Fragment> listFragments() {
