504285cccea0d814d8c3fae74f3f08ce876cb414
Sander de Winter <sanderdewinter@gamil.com>
2016-06-22 19:38:15 +0200

    graph cardio done

--- app/src/main/java/com/nwa/smartgym/fragments/main/CardioActivity.java
+++ app/src/main/java/com/nwa/smartgym/fragments/main/CardioActivity.java
@@ -6,6 +6,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -18,7 +22,10 @@ import com.nwa.smartgym.lib.DefaultPageAdapter;
 import com.nwa.smartgym.lib.ErrorHelper;
 import com.nwa.smartgym.lib.SecretsHelper;
 
-import java.util.Date;
+import org.joda.time.Duration;
+
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import retrofit2.Call;
@@ -29,6 +36,7 @@ public class CardioActivity extends Fragment {
     private CardioActivityAPI cardioActivityAPI;
     private List<com.nwa.smartgym.models.CardioActivity> sessions;
     private GraphView cardioActivityGaph;
+    private Spinner spinner;
 
     public static CardioActivity newInstance() {
         CardioActivity cardioActivity = new CardioActivity();
@@ -42,6 +50,11 @@ public class CardioActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_cardio_activity, container, false);
+
+        spinner = (Spinner) rootView.findViewById(R.id.cardio_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.graph_types, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         cardioActivityAPI = ServiceGenerator.createSmartGymService(CardioActivityAPI.class, new SecretsHelper(getContext()).getAuthToken());
         cardioActivityGaph = (GraphView) rootView.findViewById(R.id.cardio_activity_graph);
@@ -64,6 +77,17 @@ public class CardioActivity extends Fragment {
                 } else {
                     sessions = response.body();
                     plotGraph();
+
+                    spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            plotGraph();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+                        }
+                    });
                 }
             }
 
@@ -81,20 +105,48 @@ public class CardioActivity extends Fragment {
 
         cardioActivityGaph.removeAllSeries();
 
-        DataPoint[] data = new DataPoint[filteredSessions.size()];
-        for (int i = 0; i < data.length; i++) {
-            com.nwa.smartgym.models.CardioActivity session = filteredSessions.get(i);
-
-            Date date = new Date(session.getStartDate().getMillis());
-
-            data[i] = new DataPoint(date, session.getCalories());
+        DataPoint[] data;
+        if (spinner.getSelectedItem().toString().equals("Calories")) {
+            data = getCaloriesData(filteredSessions);
+        } else {
+            data = getDurationData(filteredSessions);
         }
 
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(data);
         cardioActivityGaph.addSeries(series);
+        cardioActivityGaph.getViewport().setMinY(0);
+    }
+
+    private DataPoint[] getCaloriesData(List<com.nwa.smartgym.models.CardioActivity> filteredSessions) {
+        DataPoint[] data = new DataPoint[filteredSessions.size()];
+        for (int i = 0; i < data.length; i++) {
+            Log.i("NWA", String.valueOf(filteredSessions.get(i).getCalories()));
+            data[i] = new DataPoint(i + 1, filteredSessions.get(i).getCalories());
+        }
+
+        return data;
+    }
+
+    private DataPoint[] getDurationData(List<com.nwa.smartgym.models.CardioActivity> filteredSessions) {
+        DataPoint[] data = new DataPoint[filteredSessions.size()];
+        for (int i = 0; i < data.length; i++) {
+            com.nwa.smartgym.models.CardioActivity session = filteredSessions.get(i);
+            Duration duration = new Duration(session.getStartDate(), session.getEndDate());
+
+            data[i] = new DataPoint(i + 1, duration.getStandardMinutes());
+        }
+
+        return data;
     }
 
     private List<com.nwa.smartgym.models.CardioActivity> getFilteredSessions() {
+        Collections.sort(sessions, new Comparator<com.nwa.smartgym.models.CardioActivity>() {
+            @Override
+            public int compare(com.nwa.smartgym.models.CardioActivity lhs, com.nwa.smartgym.models.CardioActivity rhs) {
+                return lhs.getStartDate().compareTo(rhs.getStartDate());
+            }
+        });
+
         return sessions;
     }
 }
